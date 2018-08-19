f8f9e04204910d0ee2d8a4a09ca2c57c02bd5247
Mariano Gappa <spinetta@gmail.com>
2018-08-19 23:00:23 +1200

    Updates pdf cv

--- update_pdf_from_current_online_cv.sh
+++ update_pdf_from_current_online_cv.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-chrome=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
-$chrome --headless --disable-gpu --print-to-pdf http://marianogappa.github.io/cv/
+function chrome {
+    "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome" "$@"
+}
+
+chrome --headless --disable-gpu --print-to-pdf http://marianogappa.github.io/cv/
 mv output.pdf cv/cv.pdf
